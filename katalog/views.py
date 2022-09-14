1593050d24aa2c585e296f99d12e4641f451b6ee
aghniya.zhafira <aghniya.zhafira@ui.ac.id>
2022-09-14 10:42:33 +0700

    Tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
 # TODO: Create your views here.
+from django.shortcuts import render
+from katalog.models import CatalogItem
+
+# Create your views here.
+def show_katalog(request):
+
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Aghniya Zhafira',
+        'npm': '2106654164'
+    }
+    return render(request,"katalog.html", context)
